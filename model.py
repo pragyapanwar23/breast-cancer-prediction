6481e85ac78201a502526604036af40dc15c9f0d
Pragya Panwar <98818480+pragyapanwar23@users.noreply.github.com>
2025-07-06 14:55:51 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,15 +3,14 @@ from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 
-
+print("📦 Starting model load...")
 model = load_model("final_model.h5")
-print("✅ Model loaded")
-
+print("✅ final_model.h5 loaded successfully")
 
 def preprocess_image(image, target_size=(224, 224)):
-    image = image.convert("RGB")  
+    image = image.convert("RGB")
     image = image.resize(target_size)
-    image_array = np.array(image) / 255.0  
+    image_array = np.array(image) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
 
@@ -19,3 +18,4 @@ def predict(image):
     processed_image = preprocess_image(image)
     prediction = model.predict(processed_image)[0]
     return prediction
+
